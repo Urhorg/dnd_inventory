a5e4e28119b5b6d036d8fdf527c85cd79fe8b27f
BSalvador <urhorg@gmail.com>
2022-11-15 23:22:30 +0100

    .

--- inventory.js
+++ inventory.js
@@ -1,20 +1,7 @@
-var upload=document.getElementById('upload');
-upload.onchange = function(uploadFle) {
-  if (!window.FileReader) {
-    alert('Your browser does not support HTML5 "FileReader" function required to open a file.');
-  } else {
-    let fileis = upload.files[0];
-    let fileredr = new FileReader();
-    fileredr.onload = function (fle) {
-      let arrayBuffer=fle.target.result;
-      let uInt8Array=new Uint8Array(arrayBuffer);
-      let db=new SQL.Database(uInt8Array);
-    };
-    fileredr.readAsArrayBuffer(fileis);
-  }
-};
-
 let field = document.getElementById("field1");
-field.textContent = "0";
+field.textContent = db.exec("select * from items")
+
+
+
 
 
